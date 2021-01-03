5462197eb74836d38b9901c8b3bf5ecfaa128166
Ghvstcode <impoissants@gmail.com>
2021-01-03 08:55:02 +0100

    save current state

--- examples/main.go
+++ examples/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 
 	c := twillight.NewClient("ACd4859955d9ff9fb86b0a6daabd2bd699", "f7c9a17f472979d2841c0d7a5e7495c6")
-	res, err := c.NewOutgoingMessage("+2347032541112", "+16592045850", "Hello Tobi", twillight.OptProvideFeedback(true))
+	res, err := c.NewOutgoingMessage("+2347", "+16592045850", "Hello Tobi", twillight.OptProvideFeedback(true))
 
 	fmt.Println("Authorization", c.Client.BasicAuth)
 
