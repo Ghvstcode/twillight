7b87bea95cb19c4c6b534b1e60b3ab7bc47669b1
Ghvstcode <impoissants@gmail.com>
2021-01-25 20:07:35 +0100

    NewOutgoingWhatsappMessage Test

--- sms_test.go
+++ sms_test.go
@@ -40,7 +40,28 @@ func(m *MockSmsService) InternalNewOutgoingMessage(to string, from string, msgbo
 	}, nil
 }
 func(m *MockSmsService)InternalNewOutgoingWhatsappMessage(to string, from string, msgbody string, opts utils.SmsOpts) (*sms.ResponseSms, error){
+	if m.Err != nil {
+		return nil, errors.New("an Error Occurred, Unable to complete verification")
+	}
 
+	if to == "" {
+		return nil, errors.New("invalid TO number")
+	}
+
+	if from == "" {
+		return nil, errors.New("invalid FROM number")
+	}
+
+	return &sms.ResponseSms{
+		AccountSid: m.AccountSID,
+		Status: "sent",
+		Body: msgbody,
+		From: from,
+		ErrorCode: nil,
+		To: to,
+		Sid: m.Sid,
+		URI: "/2010-04-01/Accounts/" + m.AccountSID + "/Messages/" + m.Sid+ ".json",
+	}, nil
 }
 func(m *MockSmsService)InternalNewOutgoingMediaMessage(to string, from string, msgbody string, mediaurl string, opts utils.SmsOpts) (*sms.ResponseSms, error){
 
@@ -100,6 +121,63 @@ func TestNewOutgoingMessage(t *testing.T) {
 
 	for _, c := range cases {
 		res, err := twillight.NewOutgoingMessage(&c.m, c.to, c.from, c.body)
+		if !reflect.DeepEqual(err, c.ExpectedErr) {
+			t.Errorf("Expected err to be %q but it was %q", c.ExpectedErr, err)
+		}
+		if res != nil {
+			if c.ExpectedSid != res.Sid {
+				t.Fatalf("Expected SID to be %s but got %s", c.ExpectedSid, res.Sid)
+			}
+
+			if c.ExpectedAccountSid != res.AccountSid {
+				t.Fatalf("Expected AccountSID to be %s but got %s", c.ExpectedAccountSid, res.AccountSid)
+			}
+
+			if c.ExpectedErrorCode != res.ErrorCode {
+				t.Fatalf("Expected ErrorCode to be %s but got %s", c.ExpectedErrorCode, res.ErrorCode)
+			}
+
+			if c.ExpectedURL != res.URI {
+				t.Fatalf("Expected URL to be %s but got %s", c.ExpectedURL, res.URI)
+			}
+		}
+	}
+
+}
+
+func TestNewOutgoingWhatsappMessage(t *testing.T) {
+	cases := [] struct{
+		m MockSmsService
+		to string
+		from string
+		body string
+		ExpectedErr error
+		ExpectedStatus string
+		ExpectedURL string
+		ExpectedSid string
+		ExpectedAccountSid string
+		ExpectedErrorCode string
+	}{
+		{
+			m: MockSmsService{
+				Err: nil,
+				Sid: "12345",
+				AccountSID: "4444444",
+			},
+			to: "987654321",
+			from: "000009999777755",
+			body: "Hello World",
+			ExpectedSid: "12345",
+			ExpectedErr: nil,
+			ExpectedStatus: "sent",
+			ExpectedErrorCode: nil,
+			ExpectedURL: "/2010-04-01/Accounts/4444444/Messages/12345.json",
+
+		},
+	}
+
+	for _, c := range cases {
+		res, err := twillight.NewOutgoingWhatsappMessage(&c.m, c.to, c.from, c.body)
 		if !reflect.DeepEqual(err, c.ExpectedErr) {
 			t.Errorf("Expected err to be %q but it was %q", c.ExpectedErr, err)
 		}
