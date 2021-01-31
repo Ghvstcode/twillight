0ef6f9cdb290e2521afed74f8805eb7fa639dfd5
Ghvstcode <impoissants@gmail.com>
2021-01-31 23:06:31 +0100

    Verify Example

---
+++ examples/ExampleVerify.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GhvstCode/twillight"
+	"github.com/GhvstCode/twillight/internal/verify"
+)
+
+func exampleVerifyService(auth *twillight.Auth,FriendlyName string, opts twillight.VerOptions)(*verify.ResponseVerifyService, error){
+	exampleNewVerifyService, err := auth.NewVerificationService(FriendlyName, opts)
+	if err != nil{
+		fmt.Println(err)
+	}
+	return exampleNewVerifyService, nil
+}
+
+func exampleVerify(svc *verify.ResponseVerifyService, to, channel string){
+	v, err := twillight.StartVerification(svc, to, channel)
+	if err != nil{
+		fmt.Println(err)
+	}
+	fmt.Println(v.Sid)
+}
+
+func exampleConfirmVerify(svc *verify.ResponseVerifyService, to, code string)string{
+	res, err := twillight.CompleteVerification(svc, to, code)
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	return res.Status
+}
